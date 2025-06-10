d12fd1b959e4230729ec68acdbfc256307367471
Joao Paulo Cirilo Dias <joao.dias@vitru.com.br>
2025-06-10 17:27:45 -0300

    MATRIX ADDITION

---
+++ src/utils/matrix.py
@@ -0,0 +1,58 @@
+from utils.osrm import table as osrm_table
+from utils.ors import table as ors_table
+
+def round_to_cost(d):
+    return int(d + 0.5)
+
+def get_index(locations, locations_indices, loc):
+    new_index = len(locations)
+
+    lon_str = str(loc[0])
+    lat_str = str(loc[1])
+
+    if lon_str in locations_indices:
+        if lat_str in locations_indices[lon_str]:
+            return locations_indices[lon_str][lat_str]
+        else:
+            locations_indices[lon_str][lat_str] = new_index
+    else:
+        locations_indices[lon_str] = {lat_str: new_index}
+    
+    locations.append(loc)
+    return new_index
+
+def add_matrices(data, routing):
+    if "engine" not in routing or (
+        routing["engine"] != "ors" and routing["engine"] != "osrm"
+    ):
+        raise ValueError("Routing engine must be 'ors' or 'osrm'.")
+
+    locs = []
+    index_of_known_locations = {}
+
+    profiles = set()
+
+    for v in data["vehicles"]:
+        if "profile" in v:
+            profiles.add(v["profile"])
+        else:
+            profile.add("car")
+
+        if ("start" not in v) and ("end" not in v):
+            raise ValueError("Missing coordinates for vehicle.")
+        
+        if "start" in v:
+            v["start_index"] = get_index(locs, index_of_know_locations, v["start"])
+        if "end" in v:
+            v["end_index"] = get_index(locs, index_of_know_locations, v["end"])
+    
+    if "jobs" in data:
+        for job in data["jobs"]:
+            if "location" not in job:
+                raise Valueerror("Missing coordinates for job.")
+            else:
+                job["location_index"] = get_index(
+                    locs, index_of_know_locations, job["location"]
+                )
+
+    if "shipments" in data:
---
+++ src/utils/osm.py
@@ -0,0 +1,6 @@
+import requests
+
+DEFAULT_IP = "0.0.0.0"
+DEFAULT_PORT = "5000"
+
+def format_request(service, locs, ip=DEFAULT_IP, port=DEFAULT_PORT):
