235ab93ce9812894e76eba645580ab9a18126d97
Joao Paulo Cirilo Dias <joaopaullo.diass@gmail.com>
2025-06-17 15:10:48 -0300

    requests

--- src/utils/ors.py
+++ src/utils/ors.py
@@ -1,12 +1,12 @@
 import json
-import request
+import requests
 
 DEFAULT_IP = "0.0.0.0"
 DEFAULT_PORT = "8080"
 
 def table(locs, profile, ip=DEFAULT_IP, port=DEFAULT_PORT):
     url = "http://" + ip + ":" + port + "ors/v2/matrix" + profile
-    req = request.post(
+    req = requests.post(
         url,
         data = json.dumps({"locations": locs}),
         headers = {"Content-Type": "application/json"}, 
