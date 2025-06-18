c3757ca833272e030041f6c54514e02c285b0cdd
Joao Paulo Cirilo Dias <joaopaullo.diass@gmail.com>
2025-06-18 16:37:15 -0300

    ATT 2.3

--- src/global_indicators.py
+++ src/global_indicators.py
@@ -52,4 +52,3 @@ if __name__ == "__main__":
         r["computing_times"]["routing"],
         s,
     )
-
---
+++ src/hvrp_to_json.py
@@ -0,0 +1,33 @@
+import json
+import os
+from utils.benchmark import get_matrix
+
+CUSTOM_PRECISION = 1000
+FIRST_LINE = 5
+
+def parse_meta(line):
+    meta = line.split()
+    return {
+        "JOBS": int(meta[0]),
+        "VEHICLES_TYPES": int(meta[1]),
+        "LOWER_BOUND": int(meta[4]),
+        "BKS": float(meta[5]),
+    }
+
+def parse_jobs(lines, jobs, coords):
+    for i in range(len(lines)):
+        customer = lines[i].split()
+        if len(customer) < 3:
+            print("Too few fields in job line")
+            exit(2)
+
+        current_coords = [int(customer[0], int(customer[1]))]
+        jobs.append(
+            {
+                "id": i,
+                "location": current_coords,
+                "location_index": len(coords),
+                "delivery": [int(customer[2])],
+            }
+        )
+        coords.append(current_coords)
