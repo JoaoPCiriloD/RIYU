d82bfef6d91bc02103ee23d4c597fe5592df6bbf
Joao Paulo Cirilo Dias <joaopaullo.diass@gmail.com>
2025-06-18 10:21:51 -0300

    att JSON

--- src/cvrplib_to_json.py
+++ src/cvrplib_to_json.py
@@ -37,5 +37,93 @@ def parse_cvrp(input_file):
     meta["CAPACITY"] = int(meta["CAPACITY"])
 
     node_start = next(
-        
+        (i for i, s in enumerate(lines) if s.startswith("NODE_COORD_SECTION")),
     )
+
+    jobs = []
+    coords = []
+
+    for i in range(node_start + 1, node_start + 1 + meta["DIMENSION"]):
+        node = parse_node_coords(lines[i])
+        if node:
+            coords.append(node["location"])
+            jobs.append(
+                {
+                    "id": node["id"],
+                    "location": node["location"],
+                    "location_index": node["id"] - 1,
+                    "type": node["type"],                    
+                }
+            )
+    total_delivry = 0
+    total_pickup = 0
+    demand_start = next(
+        (i for i, s in enumerate(lines) if s.startswith("DEMAND_SECTION")),
+    )
+    for i in range(demand_start + 1, demand_start + 1 + meta["DIMENSION"]):
+        demand_line = parse_demand(lines[i])
+        if len(demand_line) < 2:
+            break
+        job_id = int(demand_line[0])
+        current_demand = int(demand_line[1])
+
+        for j in jobs:
+            if j["id"] == job_id:
+                if j["type"] == "linehaul":
+                    if j["type"] == "pickup":
+                        j["delivery"] = [current_demand]
+                        total_delivery += current_demand
+                    elif j["type"] == "backhaul":
+                        j["pickup"] = [current_demand]
+                        total_pickup += current_demand
+
+                    j.pop("type")
+                    break
+    depot_start = next(
+        (i for i, s in enumerate(lines) if s.startswith("DEPOT_SECTION")),
+    )
+
+    depot_def = lines[depot_start + 1].strip()
+    if len(depot_def) == 1:
+        depot_loc = [float(depot_def[0]), float(depot_def[1])]
+        depot_index = len(coords)
+        coords.append(depot_loc)
+    else:
+        depot_id = int(depot_def[0])
+        job_index = next((i for i, j in enumerate(jobs) if j["id"] == depot_id))
+        depot_loc = jobs[job_index]["location"]
+        depot_index = jobs[job_index]["location_index"]
+        jobs.pop(job_index)
+
+    matrix = get_matrix(coords)
+
+    if "VEHICLES" in meta:
+        meta["VEHICLES"] = int(meta["VEHICLES"])
+        nb_vehicles = meta["VEHICLES"]
+    else:
+        if "X" in meta["NAME"]:
+            nb_vehicles = int (
+                1
+                +(
+                    VEHICLE_MARGIR_FACTOR
+                    * max(total_delivery + total_pickup)
+                    / meta["CAPACITY"]
+                )
+            )
+        else:
+            nb_vehicles = int(meta["NAME"][meta["NAME"].index("X") + 1 :])
+
+    is_VRPB = (total_pickup != 0) or (total_delivery != 0)
+
+    vehicles = []
+    for i in range(nb_vehicles):
+        vehicles.append(
+            {
+                "id": i,
+                "start": depot_loc,
+                "start_index": depot_index,
+                "end": depot_loc,
+                "end_index": depot_index,
+                "capacity": meta["CAPACITY"],
+            }
+        )
