1a7a7c3d81336ab5bcec1a270fcc0935d9877977
Joao Paulo Cirilo Dias <joaopaullo.diass@gmail.com>
2025-06-25 11:42:58 -0300

    OB3

--- src/overpass_to_json.py
+++ src/overpass_to_json.py
@@ -14,27 +14,113 @@ def name_if_present(n):
             description="Convert Overpass API data to JSON format."                             
         )
         parser.add_argument(
-            "-c",
-            "--city",
-            metavar="CITY",
-            help="City to restrict overpass query to",
-            default=None,
+        "-c",
+        "--city",
+        metavar="CITY",
+        help="city to restrict overpass query to",
+        default=None,
         )
         parser.add_argument(
-            "-o",
-            "--output",
-            metavar="OUTPUT",
-            help="Output file name (default: output.json)",
-            default="output.json",
+            "-o", "--output", metavar="OUTPUT", help="output file name", default=None
         )
         parser.add_argument(
-            "-f",
-            "--format",
-            choices=["json", "geojson", "csv"],
-            default="json",
-            help="Output format (default: json)",
+            "-k",
+            "--key",
+            metavar="KEY",
+            help="key value to use in overpass query",
+            default="amenity",
         )
-        args = parser.parse_args()
-        if args.city is None:
-            print("Please specify a city using the -c or --city option.")
-            sys.exit(1)
+        parser.add_argument(
+            "-v",
+            "--value",
+            metavar="VALUE",
+            help="comma-separated list of value(s) to use in overpass query",
+            default="cafe",
+        )
+        parser.add_argument(
+            "--top",
+            metavar="TOP",
+            help="bounding box max latitude",
+            type=float,
+            default=48.86,
+        ),
+        parser.add_argument(
+            "--bottom",
+            metavar="BOTTOM",
+            help="bounding box min latitude",
+            type=float,
+            default=48.85,
+        ),
+        parser.add_argument(
+            "--left",
+            metavar="LEFT",
+            help="bounding box min longitude",
+            type=float,
+            default=2.37,
+        ),
+        parser.add_argument(
+            "--right",
+            metavar="RIGHT",
+            help="bounding box max longitude",
+            type=float,
+            default=2.39,
+        ),
+        parser.add_argument(
+            "--geojson",
+            action="store_true",
+            help="also write a geojson file with all generated points",
+            default=False,
+        )
+        parser.add_argument(
+            "--csv",
+            action="store_true",
+            help="also write a csv file with coordinates for all generated points",
+            default=False,
+        )
+
+        argparse = parser.parse_args()
+        file_name = argparse.output
+        values = args.value.split(",")
+        values_name = "_".join(values)
+
+        if args.city is not None:
+            if not file_name:
+                file_name = f"{args_city}_{args.key}_{values_name}.json"
+            nodes = node_coordinates_city(args.key, values, args.city)["elemenets"]
+        else:
+            if not file_name:
+                file_name = (
+                    args.key
+                    + "_"
+                    + values_name
+                    + "_"
+                    + str(args.bottom)
+                    + "_"
+                    + str(args.left)
+                    + "_"
+                    + str(args.top)
+                    + "_"
+                    + str(args.right)
+                )
+
+            nodes = node_coordinates_bb(
+                args.key, values, args.bottom, args.left, args.top, args.right
+            )["elements"]
+
+        if len(nodes) == 0:
+            print("No nodes found with the given parameters.")
+            sys.exit(0)
+
+        lons = map(lambda n: n["lon"], nodes)
+        lats = map(lambda n: n["lat"], nodes)
+        names = map(lambda n: name_if_present(n), nodes)
+
+        locations = {
+            "vehicles": {"coordinates": [[lons[0], lats[0]]], "names": [names[0]]},
+            "jobs": {
+                "coordinates": [list(c) for c in zip(lons[1:], lats[1:])],
+                "names": names[1:],
+            }
+        }
+
+        write_files(file_name, locations, args.geojson, args.csv)
