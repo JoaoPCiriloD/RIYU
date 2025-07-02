e486231dbb11d7150fae4e0ae408acc924d06827
Joao Paulo Cirilo Dias <joaopaullo.diass@gmail.com>
2025-07-02 11:36:32 -0300

    ON8

--- src/random_problem.py
+++ src/random_problem.py
@@ -19,6 +19,23 @@ def generate_random_problem(j, s, v, center, sw, ne, file_name, uniform, geojson
         for i in range(j):
             j_lon = round(npr.uniform(sw[0], ne[0], 1)[0], 5)
             j_lat = round(npr.uniform(sw[1], ne[1], 1)[0], 5)
-            v_loc = [v_lon, v_lat]
+            locations["jobs"]["coordinates"].append([j_lon, j_lat])
 
-            
+        locations["shipments"] = {"coordinates": []}
+        for i in range(2 * s):
+            s_lon = round(npr.uniform(sw[0], ne[0], 1)[0], 5)
+            s_lat = round(npr.uniform(sw[1], ne[1], 1)[0], 5)
+            locations["shipments"]["coordinates"].append([s_lon, s_lat])
+
+    else:
+        mu_lon = (sw[0] + ne[0]) / 2
+        mu_lat = (sw[1] + ne[1]) / 2
+        sigma_lon = (ne[0] - mu_lon) / 3
+        sigma_lat = (ne[1] - mu_lat) / 3
+
+        if center:
+            v_loc = ()
+        else:
+            v_lon = round(npr.normal(mu_lon, sigma_lon, 1)[0], 5)
+            v_lat = round(npr.normal(mu_lat, sigma_lat, 1)[0], 5)
+            v_loc = [v_lon, v_lat]
