e09740c763eaa1325ce16a58af0caeec8e0840aa
Joao Paulo Cirilo Dias <joaopaullo.diass@gmail.com>
2025-07-03 15:44:45 -0300

    OB13

--- src/random_problem.py
+++ src/random_problem.py
@@ -50,7 +50,135 @@ def generate_random_problem(j, s, v, center, sw, ne, file_name, uniform, geojson
             locations["jobs"]["coordinates"].append([j_lon, j_lat])
         
         locations["shipments"] = {"coordinates": []}
+        for i in range(2 * s):
+            s_lon = round(npr.normal(mu_lon, sigma_lon, 1)[0], 5)
+            s_lat = round(npr.normal(mu_lat, sigma_lat, 1)[0], 5)
+            locations["shipments"]["coordinates"].append([s_lon, s_lat])
 
-        
-         
-        
+        write_files(file_name, locations, geojson, csv)
+
+        if __name__ == "__main__":
+            parser = argparse.ArgumentParser(description="Generate random problem")
+            parser.add_argument(
+                "-c",
+                "--center",
+                action="store_true",
+                help="center vehicles location in the bbox",
+                default=False,
+            )
+            parser.add_argument(
+                "-j",
+                "--jobs",
+                metavar="JOBS",
+                help="number of jobs to generate",
+                type=int,
+                default="50",
+            )
+            parser.add_argument(
+                "-s",
+                "--shipments",
+                metavar="SHIPMENTS",
+                help="number of shipments to generate",
+                type=int,
+                default="0",
+            )
+            parser.add_argument(
+                "-v",
+                "--vehicles",
+                metavar="VEHICLES",
+                help="number of vehicles to generate",
+                type=int,
+                default="5",
+            )
+            parser.add_argument(
+                "-o", "--output", metavar="OUTPUT", help="output file name", default=None
+            )
+            parser.add_argument(
+                "--top",
+                metavar="TOP",
+                help="bounding box max latitude",
+                type=float,
+                default=49.5,
+            ),
+            parser.add_argument(
+                "--bottom",
+                metavar="BOTTOM",
+                help="bounding box min latitude",
+                type=float,
+                default=48,
+            ),
+            parser.add_argument(
+                "--left",
+                metavar="LEFT",
+                help="bounding box min longitude",
+                type=float,
+                default=1.4,
+            ),
+            parser.add_argument(
+                "--right",
+                metavar="RIGHT",
+                help="bounding box max longitude",
+                type=float,
+                default=3.5,
+            ),
+            parser.add_argument(
+                "-r",
+                "--random_seed",
+                metavar="RANDOM_SEED",
+                help="number used for seeding the random generation",
+                type=int,
+                default=14,
+            )
+            parser.add_argument(
+                "--uniform",
+                action="store_true",
+                help="use an uniform distribution (default is normal)",
+                default=False,
+            )
+            parser.add_argument(
+                "--geojson",
+                action="store_true",
+                help="also write a geojson file with all generated points",
+                default=False,
+            )
+            parser.add_argument(
+                "--csv",
+                action="store_true",
+                help="also write a csv file with coordinates for all generated points",
+                default=False,
+            )
+
+            args = parser.parse_args()
+
+            # Set random seed for generation.
+            npr.seed(args.random_seed)
+
+            file_name = args.output
+            j = args.jobs
+            s = args.shipments
+            v = args.vehicles
+
+            if not file_name:
+                file_name = ""
+                if j > 0:
+                    file_name += "j_" + str(j) + "_"
+                if s > 0:
+                    file_name += "s_" + str(s) + "_"
+
+                file_name += "v_" + str(v) + "_seed_" + str(args.random_seed)
+
+                if args.center:
+                    file_name += "_center"
+
+            generate_random_problem(
+                j,
+                s,
+                v,
+                args.center,
+                [args.left, args.bottom],
+                [args.right, args.top],
+                file_name,
+                args.uniform,
+                args.geojson,
+                args.csv,
+            )
